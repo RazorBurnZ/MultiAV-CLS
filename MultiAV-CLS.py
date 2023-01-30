8b80368258e13e74f15fa3d509cfc52fc4d82bfa
RazorBurnZ <31613583+RazorBurnZ@users.noreply.github.com>
2023-01-30 18:26:49 +0800

    Enhanced the Main Menu
    * Main Menu changes
    * Added External AV GUI tools (Added KVRT and DrWeb CureIt as external tools)

--- MultiAV-CLS.py
+++ MultiAV-CLS.py
@@ -5,21 +5,28 @@ import zipfile
 import requests
 import re
 import hashlib
+import pyfiglet
+
+def display_menu():
+    os.system("clear")
+    header = "MULTIAV-CLS"
+    sub_header = "Multiple AntiVirus - Command Line Scanner"
+    ascii_banner = pyfiglet.figlet_format(header, font = "big")
+    banner_lines = ascii_banner.split("\n")
+    print("\n".join(banner_lines))
+    print("\n" + sub_header.center(len(sub_header)))
+    print("\nMain Menu:".center(len("Main Menu:")))
+    print("\nPlease select an option:".center(len("Please select an option:")))
+    print("1. Input a scan folder to scan with several AV Scanners".center(len("1. Input a scan folder to scan with several AV Scanners")))
+    print("2. Update virus definition".center(len("2. Update virus definition")))
+    print("3. External Tools".center(len("3. External Tools")))
+    print("4. Others".center(len("4. Others")))
+    print("5. Exit".center(len("5. Exit")))
 
 
-ScanDir = ""
-base_directory = os.getcwd()
-Logs_directory = os.getcwd() + '\Logs'
-print('Base working directory : ', base_directory)
-print('Get current Logs directory : ', Logs_directory)
 while True:
-    print("Menu:")
-    print("1. Input directory to scan")
-    print("2. Update virus definitions")
-    print("3. Parse logs")
-    print("4. Exit")
+    display_menu()
     choice = input("Enter your choice: ")
-
     if choice == "1":
         ScanDir = input("Enter the directory path: ")
         if os.path.isdir(ScanDir):
@@ -28,7 +35,6 @@ while True:
                 shutil.rmtree(Logs_directory)
             os.makedirs(Logs_directory)
             
-              
             command1 = "Avira\\scancl.exe /a /z --withtype=all --heurlevel=3 --log="+ base_directory + "\Logs\AviraLog.log --logformat=singleline --stats " + ScanDir
             print(command1)
             subprocess.run(command1, shell=True)
@@ -68,14 +74,11 @@ while True:
             print(command8)
             subprocess.run(command8, shell=True)
             print("RMS scan has been completed.")
-            
-            
         else:
             print("The directory does not exist.")
-
+        display_menu()
     elif choice == "2":
-        # Update virus definitions
-        
+        # Update virus definitions      
         # Updating Avira Virus definitions
         subprocess.run("Avira/Fusebundle/fusebundle.exe")
 
@@ -89,43 +92,63 @@ while True:
         for file in os.listdir(temp_dir):
             src_file = os.path.join(temp_dir, file)
             dst_file = os.path.join("Avira/", file)
-            shutil.move(src_file, dst_file)
-    
-        print("Avira virus definitions have been updated.")
-
-        
+            shutil.move(src_file, dst_file)    
+        print("Avira virus definitions have been updated.")        
         # Updating ClamAV Virus definitions
         subprocess.run("ClamAV/freshclam.exe")
-        print("ClamAV virus definitions have been updated.")
-        
-        
+        print("ClamAV virus definitions have been updated.")            
         # Updating Comodo Cleaning Essentials Virus definitions
         subprocess.run("CCE/CCE.exe -u")
-        print("Comodo Cleaning Essentials virus definitions have been updated.")
-       
+        print("Comodo Cleaning Essentials virus definitions have been updated.")      
         # Updating Emsisoft Virus definitions
         subprocess.run("EEK/bin64/a2cmd.exe /u")
-        print("Emsisoft virus definitions have been updated.")
-             
+        print("Emsisoft virus definitions have been updated.")             
         # Updating Ikarus Virus definitions
         subprocess.run(["update-ikarus.bat"])
-         print("Ikarus virus definitions have been updated.")
-         
+        print("Ikarus virus definitions have been updated.")         
         # Updating McAfee Virus definitions
         subprocess.run(["update-mcafee.bat"])
-        print("McAfee DAT files have been updated.")
-
+        print("McAfee DAT files have been updated.")        
+        display_menu()
         
     elif choice == "3":
-        # Parse logs
-        subprocess.run("CheckEnc.bat", shell=True)
-        subprocess.run("parselogs.py", shell=True)
-        print("Parsing Antivirus Scan Logs.")
-        pass
-        
+        while True:
+            os.system("clear")
+            header = "MULTIAV-CLS"
+            sub_header = "Multiple AntiVirus - Command Line Scanner"
+            ascii_banner = pyfiglet.figlet_format(header, font = "big")
+            banner_lines = ascii_banner.split("\n")
+            print("\n".join(banner_lines))
+            print("\n" + sub_header.center(len(sub_header)))
+            print("\nSub Menu: External GUI Tools".center(len("Sub Menu: External GUI Tools")))
+            print("\nPlease select an option:".center(len("Please select an option:")))
+            print("1. Open Kaspersky Virus Removal Tool")
+            print("2. DrWeb CureIt")
+            print("3. Comodo Cleaning Essentials")
+            print("4. Emsisoft Emergency Kit")
+            print("5. Back to main menu")
+            sub_choice = input("Enter your choice: ")
+            if sub_choice == "1":
+                subprocess.run("Tools/KVRT.exe")
+            elif sub_choice == "2":
+                subprocess.run("Tools/drwebci.exe")
+            elif sub_choice == "3":
+                subprocess.run("CCE/CCE.exe")
+            elif sub_choice == "4":
+                subprocess.run("eek/Start Scanner.exe")
+            elif sub_choice == "5":
+                break
+            else:
+                print("Invalid choice. Please enter a number between 1 and 5.")
+
     elif choice == "4":
+        print("Others")
+        display_menu()
+        
+    elif choice == "5":
         print("Exiting...")
         break
     else:
-        print("Invalid choice. Please enter a number between 1 and 4.")
+        print("Invalid choice. Please enter a number between 1 and 5.")
+
 
